554b4fdfe3d41ffd286d1a29759036a017af4d91
ani67h <ghoshanindya4589@gmail.com>
2025-07-04 00:19:00 +0600

    circumference

---
+++ circumference.py
@@ -0,0 +1,8 @@
+radius = int(input("Enter the radius of the circle (in metres) : "))
+result = 0
+
+def circumference():
+  result = 2 * 3.1415926536 * radius
+  print("The circumference of the circle is ",result, "metres")
+
+circumference()
