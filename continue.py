a86eb858c4e5dfd84e610433d96c777e6214dc43
ani67h <ghoshanindya4589@gmail.com>
2025-07-05 21:02:43 +0600

    keywords

---
+++ continue.py
@@ -0,0 +1,7 @@
+var = 10           #intialise
+while var > 0:    #iterate loop
+    var = var - 1
+    if var == 5:   #conditional 1
+        continue   #continue statement
+    print("\nCurrent varriable value : ",var)
+print("\nGood bye!")
---
+++ pass.py
@@ -0,0 +1,7 @@
+a = 10
+b = 20
+if (a < b):
+    pass
+    print("B is greater than A")
+else:
+    print("B is not greater than A")
